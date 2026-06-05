f24355937b9533f3ecb9a15f52b36dbdde78baf4
caioRafael <caiorafaelrg@gmail.com>
2026-06-04 23:12:40 -0300

    feat(editor-store): add workspace and file state management

--- src/renderer/src/interface/editor.interface.ts
+++ src/renderer/src/interface/editor.interface.ts
@@ -1,4 +1,10 @@
+import { FileNode } from './FileNode'
+
 export interface EditorState {
+  workspace: FileNode | null
+  setWorkspace: (workspace: FileNode | null) => void
   code: string
   setCode: (code: string) => void
+  file: string | null
+  setFile: (file: string) => void
 }
--- src/renderer/src/store/editor.store.ts
+++ src/renderer/src/store/editor.store.ts
@@ -1,8 +1,13 @@
 import { create } from 'zustand'
 import { DEFAULT_CODE } from '@renderer/constants/defaultCode'
 import { EditorState } from '@renderer/interface/editor.interface'
+import { FileNode } from '@renderer/interface/FileNode'
 
 export const useEditorStore = create<EditorState>((set) => ({
+  workspace: null,
+  setWorkspace: (workspace: FileNode | null) => set({ workspace }),
   code: DEFAULT_CODE,
-  setCode: (code: string) => set({ code })
+  setCode: (code: string) => set({ code }),
+  file: null,
+  setFile: (file: string | null) => set({ file })
 }))
